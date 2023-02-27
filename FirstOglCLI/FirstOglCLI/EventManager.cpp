1f903c4006e7cce2e15a0a5afc53af3f58325842
Atsuro <situniv.a26@gmail.com>
2023-02-27 21:05:35 +0900

    add : some methods(include bugs)

--- FirstOglCLI/FirstOglCLI/EventManager.cpp
+++ FirstOglCLI/FirstOglCLI/EventManager.cpp
@@ -5,43 +5,30 @@
 
 EventManager::EventManager()
 {
-  m_isL = m_isR = m_isM = false;
+    isL_ = isR_ = isM_ = false;
+
+    balls_.push_back(Ball(1.0, EVec3f(0, 0, 0), EVec3f(1, 0, 0 )));
 }
 
-class Ball
+
+void Ball::Step()
 {
-private:
-    EVec3f pos_, velo_;
-    float rad_;
-
-public:
-    Ball(double rad, const EVec3f &pos,const EVec3f &velo)
-    {
-        rad_ = rad;
-        pos_ = pos;
-        velo_ = velo;
-    };
-
-    void Step();
-    void Draw();
-    EVec3f GetPos();
-    EVec3f GetVelo();
-    EVec3f SetPos();
-};
-
-void Step()
-{
-    glTranslatef;
+    float dt = 1.0;
+    pos_ += velo_ * dt;
+    //velo_ = ;
+    glTranslated(pos_[0], pos_[1], pos_[2]);
+    DrawSphere();
+    glTranslated(-pos_[0], -pos_[1], -pos_[2]);
 }
 
-void Ball::Draw()
+void Ball::DrawSphere()
 {
     //OpenGL‚Å‹…‚ð•`‚­
-    int N = 100.0;
+    const int N = 50.0;
     glBegin(GL_TRIANGLES);
-    for (int y = pos_[1]; y < N; y++)
+    for (int y = 0; y < N; y++)
     {
-        for (int x = pos_[0]; x < N; x++)
+        for (int x = 0; x < N; x++)
         {
             double t1 = (x) / (double)N * 2.0 * M_PI;
             double t2 = (x + 1) / (double)N * 2.0 * M_PI;
@@ -65,75 +52,75 @@ void Ball::Draw()
 
 void EventManager::DrawScene()
 {
-  glLineWidth(2.0f);
-  glDisable(GL_LIGHTING);//“d‹C‚ðÁ‚·
+    glLineWidth(2.0f);
+    glDisable(GL_LIGHTING);//“d‹C‚ðÁ‚·
 
-  glEnable(GL_DEPTH_TEST);
-  //Material 
-  float   shin[1] = { 64 };
-  EVec4f  spec(1, 1, 1, 0.5);
-  EVec4f  diff(0.5f, 0.5f, 0.5f, 0.5f);
-  EVec4f  ambi(0.5f, 0.5f, 0.5f, 0.5f);
-  glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, spec.data());
-  glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diff.data());
-  glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambi.data());
-  glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, shin);
+    glEnable(GL_DEPTH_TEST);
+    //Material 
+    float   shin[1] = { 64 };
+    EVec4f  spec(1, 1, 1, 0.5);
+    EVec4f  diff(0.5f, 0.5f, 0.5f, 0.5f);
+    EVec4f  ambi(0.5f, 0.5f, 0.5f, 0.5f);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, spec.data());
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diff.data());
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambi.data());
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, shin);
 
-  glEnable(GL_LIGHTING);
-  glEnable(GL_LIGHT0);
-  glEnable(GL_LIGHT1);
-  glEnable(GL_LIGHT2);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHT1);
+    glEnable(GL_LIGHT2);
 
-  //OpenGL‚Å3–{‚Ì’¼ü‚ð•`‚­
-  glBegin(GL_LINES);
-  glColor3d(1, 0, 0); glVertex3d(0, 0, 0); glVertex3d(10, 0, 0);
-  glColor3d(0, 1, 0); glVertex3d(0, 0, 0); glVertex3d(0, 10, 0);
-  glColor3d(0, 0, 1); glVertex3d(0, 0, 0); glVertex3d(0, 0, 10);
-  glEnd();
+    //OpenGL‚Å3–{‚Ì’¼ü‚ð•`‚­
+    glBegin(GL_LINES);
+    glColor3d(1, 0, 0); glVertex3d(0, 0, 0); glVertex3d(10, 0, 0);
+    glColor3d(0, 1, 0); glVertex3d(0, 0, 0); glVertex3d(0, 10, 0);
+    glColor3d(0, 0, 1); glVertex3d(0, 0, 0); glVertex3d(0, 0, 10);
+    glEnd();
 
- 
+   
 }
 
 void EventManager::LBtnDown(int x, int y, OglForCLI* ogl)
 {
-  m_isL = true;
-  ogl->BtnDown_Trans(EVec2i(x, y)); // OpenGL‚ÌŽ‹“_‚ð‰ñ“]‚³‚¹‚é€”õ
+    isL_ = true;
+    ogl->BtnDown_Trans(EVec2i(x, y)); // OpenGL‚ÌŽ‹“_‚ð‰ñ“]‚³‚¹‚é€”õ
 }
 
 void EventManager::MBtnDown(int x, int y, OglForCLI* ogl)
 {
-  m_isM = true;
-  ogl->BtnDown_Zoom(EVec2i(x, y));
+    isM_ = true;
+    ogl->BtnDown_Zoom(EVec2i(x, y));
 }
 
 void EventManager::RBtnDown(int x, int y, OglForCLI* ogl)
 {
-  m_isR = true;
-  ogl->BtnDown_Rot(EVec2i(x, y));
+    isR_ = true;
+    ogl->BtnDown_Rot(EVec2i(x, y));
 }
 
 void EventManager::LBtnUp(int x, int y, OglForCLI* ogl)
 {
-  m_isL = false;
-  ogl->BtnUp();
+    isL_ = false;
+    ogl->BtnUp();
 }
 
 void EventManager::MBtnUp(int x, int y, OglForCLI* ogl)
 {
-  m_isM = false;
-  ogl->BtnUp();
+    isM_ = false;
+    ogl->BtnUp();
 }
 
 void EventManager::RBtnUp(int x, int y, OglForCLI* ogl)
 {
-  m_isR = false;
-  ogl->BtnUp();
+    isR_ = false;
+    ogl->BtnUp();
 }
 
 void EventManager::MouseMove(int x, int y, OglForCLI* ogl)
 {
-  if (!m_isL && !m_isR && !m_isM) return;
-  ogl->MouseMove(EVec2i(x, y));
+    if (!isL_ && !isR_ && !isM_) return;
+    ogl->MouseMove(EVec2i(x, y));
 }
 
 
--- FirstOglCLI/FirstOglCLI/EventManager.h
+++ FirstOglCLI/FirstOglCLI/EventManager.h
@@ -3,11 +3,34 @@
 #pragma unmanaged
 #include "OglForCLI.h"
 
+class Ball
+{
+private:
+    EVec3f pos_, velo_;
+    float rad_;
+
+public:
+    Ball(const double rad, const EVec3f& pos, const EVec3f& velo)
+    {
+        rad_ = rad;
+        pos_ = pos;
+        velo_ = velo;
+    };
+
+    void Step();
+    void DrawSphere();
+    EVec3f GetPos();
+    EVec3f GetVelo();
+    EVec3f SetPos();
+};
+
 class EventManager
 {
 private:
-  bool m_isL, m_isR, m_isM; //マウスボタンの状態
+  bool isL_, isR_, isM_; //マウスボタンの状態
   EventManager();
+
+  std::vector<Ball> balls_;
 
 public:
   //シングルトンクラス（see デザインパターン, シングルトン）
