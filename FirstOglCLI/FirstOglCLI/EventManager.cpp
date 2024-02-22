b31726f3876cabd425b991350a3512665aa0e9e9
Rikuto11 <al21039@shibaura-it.ac.jp>
2024-02-22 15:43:15 +0900

    sphere

--- FirstOglCLI/FirstOglCLI/EventManager.cpp
+++ FirstOglCLI/FirstOglCLI/EventManager.cpp
@@ -8,6 +8,7 @@ EventManager::EventManager()
 
 void EventManager::DrawScene()
 {
+  glEnable(GL_DEPTH_TEST);
   glLineWidth(2.0f);
   glDisable(GL_LIGHTING);//ìdãCÇè¡Ç∑
 
@@ -17,6 +18,40 @@ void EventManager::DrawScene()
   glColor3d(0, 1, 0); glVertex3d(0, 0, 0); glVertex3d(0, 10, 0);
   glColor3d(0, 0, 1); glVertex3d(0, 0, 0); glVertex3d(0, 0, 10);
   glEnd();
+
+  //glBegin(GL_TRIANGLES);
+  //glColor3d(1, 0, 0); glVertex3d(0, 0, 0); glVertex3d(10, 0, 0);
+  //glVertex3d(10, 10, 0);
+  //glEnd();
+
+  glEnable(GL_LIGHTING);
+  glEnable(GL_LIGHT0);
+  glEnable(GL_LIGHT1);
+  glEnable(GL_LIGHT2);
+
+  const int N = 30;
+  glPointSize(3);
+  glBegin(GL_TRIANGLES);
+  for (int t = 0; t < N; t++)
+  {
+	  for (int p = 0; p < N; p++)
+	  {
+		  float t1 = t / (float)N * 2 * 3.141592;
+		  float t2 = (t + 1) / (float)N * 2 * 3.141592;
+		  float p1 = p / ((float)N - 0.5f) * 3.141592;
+		  float p2 = (p + 1) / ((float)N - 0.5f) * 3.141592;
+		  glNormal3d(cos(p1) * cos(t1), cos(p1) * sin(t1), sin(p1));
+		  glVertex3d(2 * cos(p1) * cos(t1), 2 * cos(p1) * sin(t1), 2 * sin(p1));
+		  glVertex3d(2 * cos(p2) * cos(t1), 2 * cos(p2) * sin(t1), 2 * sin(p2));
+		  glVertex3d(2 * cos(p2) * cos(t2), 2 * cos(p2) * sin(t2), 2 * sin(p2));
+		  glVertex3d(2 * cos(p1) * cos(t1), 2 * cos(p1) * sin(t1), 2 * sin(p1));
+		  glVertex3d(2 * cos(p2) * cos(t2), 2 * cos(p2) * sin(t2), 2 * sin(p2));
+		  glVertex3d(2 * cos(p1) * cos(t2), 2 * cos(p1) * sin(t2), 2 * sin(p1));
+
+	  }
+  }
+  glEnd();
+
 }
 
 void EventManager::LBtnDown(int x, int y, OglForCLI* ogl)
