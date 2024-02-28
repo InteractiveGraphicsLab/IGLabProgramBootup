66257f84186b1525627495caecad9af55234966c
riku0411 <al20033@shibaura-it.ac.jp>
2024-02-28 13:23:59 +0900

    Update Ball.h

--- FirstOglCLI/FirstOglCLI/Ball.h
+++ FirstOglCLI/FirstOglCLI/Ball.h
@@ -5,8 +5,8 @@
 class Ball
 {
 private:
-    float  _radi;
-    EVec3f _pos, _rot, _velo, _veloangle;
+    EVec3f _pos, _rot;
+    float _velo, _veloangle, _radi;
     
 public:
     Ball(float radi, EVec3f& pos, EVec3f& rot, EVec3f& velo, EVec3f& veloangle);
